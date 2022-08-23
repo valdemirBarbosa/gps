ffee7b4a495f290dae5497bfb536b3a0a7d081fd
valdemir <val.b.silva7@gmail.com>
2022-08-23 07:33:49 -0300

    fazendo imprimir atestado com todas as variáveis

--- app/controllers/CertidaoController.php
+++ app/controllers/CertidaoController.php
@@ -17,31 +17,34 @@ class CertidaoController extends Controller{
           $certidao = new Certidao_Model();
 
           if($dados['dados'] = $certidao->certidao($cpf)){
+               echo "if";
+               exit;
+               
+
                foreach($dados as $d){
                     $nome = ($d[0]->nome_servidor);
                     $numeroCpf = ($d[0]->cpf);
-                    $certifica = " tem processo em andamento ";
+                    $certidao = " tem processo em andamento ";
+                $dados['dadosCertidao'] = array($nome, $numeroCpf, $certidao);
                }
           }else{
                $dados['dados'] = $certidao->pegarNome($cpf);
                foreach($dados as $d){
                     $nome = ($d[0]->nome_servidor);
                     $numeroCpf = ($d[0]->cpf);
-                    $certifica = " não responde processo atualmente neste município ";
+                    $certidao = " tem processo em andamento ";
+                $dados['dadosCertidao'] = array($nome, $numeroCpf, $certidao);
                }
           }
-
-          $var1 = "Atestamos para os devidos fins que o(a) servidor(a) ".$nome ." , CPF: ".$numeroCpf; 
-          $var2 = $certifica; 
-          $html = $var1 . $var2; 
+             //  $html = $divMargemIni . $var0 . "<br><br><br>". $var1 . $var2 . $divMargemFim; 
 
      }else{
           echo "não funcionaou";
      }
 
-              $mpdf = new \Mpdf\Mpdf();
+/*               $mpdf = new \Mpdf\Mpdf();
               $mpdf->WriteHTML($html);
               $mpdf->Output();
-  }
+ */  }
 
 }       
--- app/views/certidao/index.php
+++ app/views/certidao/index.php
@@ -2,8 +2,8 @@
 	<h1 class="titulo-pagina">Certidão</h1>
 
 </div>
-<div class="base-lista">
-	</div>
+<div class="certidao">
+<img width="60%" height="10%" src="<?php echo URL_BASE ."assets\img\logoMaio2021.JPG" ?>" width="400" height="200">	
 
 <div class="pai">  
 	<div class="filho1">  
@@ -13,5 +13,40 @@
 						<input autofocus type="number" name="cpf" value="">
 						<input type="submit" value="Gerar Atestado">
 		</form>
-	</div>
+
 </div>
+</div>
+</div>
+	<?php 
+		if(isset($dados)){
+			foreach ($dados as $d){
+				print_r($d);
+
+				foreach($dados as $c){
+					echo "<hr><hr>";
+						echo "print:  ".print_r($c[2]);	
+					echo "<hr><hr>";
+
+?>
+			<div class='margemCertidao'> 
+				<div class="cabecahoCertidao">
+					<!--<img width='600' height='50' src ='" . URL_BASE . "assets/img/logoMaio2021.jpg'>  -->
+					<img width='700' height='50' src ='<?php echo  URL_BASE . "assets/img/logoMaio2021.jpg" ?>'>";
+
+					<br><br><h3>Secretaria de Administração
+					<br><p>Comissão Permanente de Sindicância e Processos Administrativo Discliplinares
+					</p></h3>
+					
+					<br><br><br><br><h2>Atestado</h2>
+		  		</div> <!-- Fim da div cabecalho da certidao -->
+
+				<br><br><br><br>
+				<div class="CorpoTextoCertida">
+					Atestamos para os devidos fins que o(a) servidor(a) <?php echo $cert->nome_servidor ." , CPF: ".$cert->cpf; 
+					$cert->$certidao; ?> 
+				</div> <!-- Fim da div corpo do texto certidao -->
+<?php			} }}?>
+
+			</div> 
+
+
